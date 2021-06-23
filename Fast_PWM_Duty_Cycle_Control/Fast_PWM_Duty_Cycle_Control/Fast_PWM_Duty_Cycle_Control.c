01be50680d975f6379c57e46adcd559ca4f2585b
vaibhav patil <49595649+vaibhavp369@users.noreply.github.com>
2021-06-23 21:49:30 +0530

    Add files via upload

---
+++ Fast_PWM_Duty_Cycle_Control/Fast_PWM_Duty_Cycle_Control/Fast_PWM_Duty_Cycle_Control.c
@@ -0,0 +1,54 @@
+/*
+ * Fast_PWM_Duty_Cycle_Control.c
+ *
+ * Created: 18/05/2021 9:25:42 PM
+ * Author : vaibhav
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED		6
+
+void ddr_init()
+{
+	DDRD |= (1<<LED);
+	PORTD &= ~(1<<LED);
+}
+
+void led_on()
+{
+	PORTD |= (1<<LED);
+}
+
+void led_off()
+{
+	PORTD &=~(1<<LED);
+}
+
+void timer_init()
+{
+	OCR0A = 0; // 50% duty cycle
+	TCCR0A |= ( (1<<WGM01) | (1<<WGM00) | (1<<COM0A1)); // Non-inverting and Fast PWM
+	TCCR0A &=~( (1<<COM0A0) | (1<<COM0B0) | (1<<COM0B1));
+	
+	TCCR0B |= ( (1<<CS02) | (1<<CS00) ); // pre-scalar=8..... - 16MHz/8*256 = 8kHz(approx)
+	TCCR0B &=~( (1<<CS01) );
+	
+	
+}
+
+int main(void)
+{
+	ddr_init();
+	timer_init();
+	
+    while (1) 
+    {
+		OCR0A++;
+		_delay_ms(200);
+		
+    }
+}
+
